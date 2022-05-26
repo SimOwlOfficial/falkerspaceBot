501e8cfd328a6f948d65ee6b0e4b71e673d0770d
SimOwlOfficial <104221003+SimOwlOfficial@users.noreply.github.com>
2022-05-26 18:40:35 +0200

    1.5

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+ "verification": "",
+ "description": "",
+ "domain": "", // This Domain need to be without https:// or http:// (eg: hydrabot.fun not https://hydrabot.fun)
+ "google_analitics": "", 
+ "token": "OTc5MDkyODc4OTY4MTAzMDky.GVsVVj.ag-571ZV5Pi8w3W5ouMKwzJOOotB8bfLXyUUEQ",
+ "https":"https://",
+ "port":"5003",
+ "client_id":"979092878968103092",
+ "secret":"vfeSTwWQHXoHqx-8dBdLvs5rVSAnOW2J",
+ "support_server_link":"https://discord.gg/RmAmHr8FYH",
+ "bot_name":"FalkerSpace"
+}
